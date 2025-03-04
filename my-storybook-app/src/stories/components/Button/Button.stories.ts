22583213d2bb686acfdf0940d1279aafe0da711f
keriwilkins <keri.wilkins@motivcx.com>
2025-03-04 15:10:16 -0600

    button stories added

--- my-storybook-app/src/stories/components/Button/Button.stories.ts
+++ my-storybook-app/src/stories/components/Button/Button.stories.ts
@@ -3,55 +3,79 @@ import { fn } from '@storybook/test';
 
 import { Button } from './Button';
 
-// so for a component i have one components (which would be button) but many variants ()
-// primary  hover state Focus State diabled state (Boolean)  then Outline same states + Action (same sta) ? 
-
-const meta = {
+const meta: Meta<typeof Button> = {
   title: 'Components/Button',
   component: Button,
   parameters: {
     layout: 'centered',
-    design: {
-      type: 'figma',
-      url: 'https://www.figma.com/design/8xlzYN0YouU38pmP0zD6bB/Content-Lion-Design-System?node-id=8-95',
-    },
+    // design: {
+    //   type: 'figma',
+    //   url: 'https://www.figma.com/design/8xlzYN0YouU38pmP0zD6bB/Content-Lion-Design-System?node-id=8-95',
+    // },
     // zeplinLink: "https://www.figma.com/design/8xlzYN0YouU38pmP0zD6bB/Content-Lion-Design-System?node-id=8-95",
   },
   tags: ['autodocs'],
   argTypes: {
-    backgroundColor: { control: 'color' },
+    state: {
+      control: 'select',
+      options: ['default', 'hover', 'focused', 'selected', 'disabled'],
+    },
+    size: {
+      control: 'select',
+      options: ['small', 'medium', 'large'],
+    },
+    label: { control: 'text' },
+    variant: {
+      table: {
+        disable: true,
+      },
+    },
+    icon: {table: {disable: true,}}
   },
-  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked
-  args: { onClick: fn() },
-} satisfies Meta<typeof Button>;
+} 
 
 export default meta;
-type Story = StoryObj<typeof meta>;
+type Story = StoryObj<typeof Button>;
 
-// All Story Types
+export const Interactive: Story = {};
+
+export const CTA: Story = {
+  args: {
+    label: 'Button',
+    variant: 'cta',
+    state: 'default',
+    size: 'medium',
+  },
+};
 export const Primary: Story = {
   args: {
-    primary: true,
     label: 'Button',
+    variant: 'primary',
+    state: 'default',
+    size: 'medium',
   },
 };
-
-export const Secondary: Story = {
+export const Outline: Story = {
   args: {
     label: 'Button',
+    variant: 'outline',
+    state: 'default',
+    size: 'medium',
   },
 };
-
-export const Large: Story = {
+export const Borderless: Story = {
   args: {
-    size: 'large',
     label: 'Button',
+    variant: 'borderless',
+    state: 'default',
+    size: 'medium',
   },
 };
-
-export const Small: Story = {
+export const Destructive: Story = {
   args: {
-    size: 'small',
     label: 'Button',
+    variant: 'destructive',
+    state: 'default',
+    size: 'medium',
   },
 };
--- my-storybook-app/src/stories/components/Button/Button.tsx
+++ my-storybook-app/src/stories/components/Button/Button.tsx
@@ -1,38 +1,38 @@
 import React from 'react';
-
+import { FaPlus } from 'react-icons/fa';
 import './button.css';
 
-export interface ButtonProps {
-  /** Is this the principal call to action on the page? */
-  primary?: boolean;
-  /** What background color to use */
-  backgroundColor?: string;
-  /** How large should the button be? */
+export type ButtonProps = {
+  label: string; 
+  variant?: 'cta' | 'primary' | 'outline' | 'borderless' | 'destructive';
+  state?: 'default' | 'hover' | 'focused' | 'selected' | 'disabled';
   size?: 'small' | 'medium' | 'large';
-  /** Button contents */
-  label: string;
-  /** Optional click handler */
-  onClick?: () => void;
-}
+  onClick?: () => void; 
+  icon?: React.ReactNode;
+};
+
 
 /** Primary UI component for user interaction */
 export const Button = ({
-  primary = false,
+  label, 
+  variant = 'primary', 
+  state = 'default',
   size = 'medium',
-  backgroundColor,
-  label,
+  onClick,
+  icon = <FaPlus />,
   ...props
 }: ButtonProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
   return (
     <>
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={{ backgroundColor }}
+      className={`btn ${variant} ${state} ${size}`}
+      disabled={state === 'disabled'}
+      onClick={onClick}
       {...props}
     >
-      {label}
+      <span className="btn-text">{label}</span>
+      {(size === 'medium' || size === 'large') && <span className="btn-icon">{icon}</span>}
     </button>
     </>
   );
